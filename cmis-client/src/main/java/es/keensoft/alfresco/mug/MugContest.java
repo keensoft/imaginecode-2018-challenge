64a8f5e86296f207726bf25891d2cd821eed9fdc
end222 <pabloorduna98@gmail.com>
2018-09-29 12:49:31 +0200

    Finalizado reto de la taza

--- cmis-client/src/main/java/es/keensoft/alfresco/mug/MugContest.java
+++ cmis-client/src/main/java/es/keensoft/alfresco/mug/MugContest.java
@@ -1,8 +1,18 @@
+/*
+ * By C++Tangana:
+ * - Alex Sanz
+ * - Raul Logroño
+ * - Eduardo Sanchez
+ * - Pablo Orduna
+ */
 package es.keensoft.alfresco.mug;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.chemistry.opencmis.client.api.ItemIterable;
+import org.apache.chemistry.opencmis.client.api.QueryResult;
+import org.apache.chemistry.opencmis.commons.data.PropertyData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -26,11 +36,21 @@ public class MugContest {
 		
 		// Hint 1: Use cmisClient.getSession().query(..) method
 		// Hint 2: Documents are identified with the property cmis:name in CMIS
-		
-		// Your code here!
-		
-		return pdfDocuments;
-		
-	}
+	//  OR (UPPER(cmis:name) LIKE UPPER('%cryptography%') OR (UPPER(cmis:name) LIKE UPPER('%neuralnetworks%') OR (UPPER(cmis:name) LIKE UPPER('%introduction%'))", false);	
+		ItemIterable<QueryResult> results = cmisClient.getSession().query("SELECT cmis:name FROM ksic:Thesis",false);
 
+		for(QueryResult hit: results) {  
+		    for(PropertyData<?> property: hit.getProperties()) {
+		    	String queryName = property.getFirstValue().toString();
+		        //String queryName = property.getFirstValue();
+		        char[] splitted = queryName.toCharArray();
+		        int i = splitted.length;
+		        if (splitted[i-1] == 'f' && splitted[i-2] == 'd' && splitted[i-3] == 'p') {
+		        	pdfDocuments.add(queryName);
+		        }
+		    }
+		}
+
+		return pdfDocuments;
+	}
 }
